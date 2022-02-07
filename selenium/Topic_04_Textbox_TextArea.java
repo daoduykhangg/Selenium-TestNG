5e6764b1b474ab2bff638081ab9d480ea49a79a3
KhangD <Khang.DaoD@homecredit.vn>
2022-02-07 15:42:16 +0700

    Textbox/TextArea finished

---
+++ selenium/Topic_04_Textbox_TextArea.java
@@ -0,0 +1,151 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class Topic_04_Textbox_TextArea {
+	WebDriver driver;
+	JavascriptExecutor js;
+	String projectPath = System.getProperty("user.dir");
+	String loginPageUrl = "http://demo.guru99.com/v4";
+	String email, userId, password, customerId;
+	String customerName, gender, dob, addressInput, addressOutput, city, state, pin, phoneNum;
+	String editAddressInput, editAddressOutput, editCity, editState, editPin, editPhone, editEmail;
+
+	@BeforeClass
+	public void beforeClass() {
+		System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+		driver = new FirefoxDriver();
+		js = (JavascriptExecutor) driver;
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get(loginPageUrl);
+		email = "khangdao" + randomNumber() + "@hcvn.vn";
+		customerName = "Khang Dao";
+		gender = "male";
+		dob = "1996-07-02";
+		addressInput = "Dang Minh Tru\nPhuong 10";
+		addressOutput = "Dang Minh Tru Phuong 10";
+		city = "HCM";
+		state = "VN";
+		pin = "123456";
+		phoneNum = "0123456789";
+
+		editAddressInput = "Bui The My\nPhuong 11";
+		editAddressOutput = "Bui The My Phuong 11";
+		editCity = "HN";
+		editState = "VietNam";
+		editPin = "987456";
+		editPhone = "0987654321";
+		editEmail = "khangdao" + randomNumber() + "@hcvn.com";
+	}
+
+	@Test
+	public void TC_01_Register() {
+		driver.findElement(By.xpath("//a[text()='here']")).click();
+		driver.findElement(By.name("emailid")).sendKeys(email);
+		driver.findElement(By.name("btnLogin")).click();
+		userId = driver.findElement(By.xpath("//td[text()='User ID :']/following-sibling::td")).getText();
+		password = driver.findElement(By.xpath("//td[text()='Password :']/following-sibling::td")).getText();
+	}
+
+	@Test
+	public void TC_02_Login() {
+		driver.get(loginPageUrl);
+		driver.findElement(By.name("uid")).sendKeys(userId);
+		driver.findElement(By.name("password")).sendKeys(password);
+		driver.findElement(By.name("btnLogin")).click();
+		Assert.assertEquals(driver.findElement(By.cssSelector(".heading3")).getText(), "Welcome To Manager's Page of Guru99 Bank");
+		Assert.assertEquals(driver.findElement(By.cssSelector("tr.heading3")).getText(), "Manger Id : " + userId);
+	}
+
+	@Test
+	public void TC_03_New_Customer() {
+		driver.findElement(By.xpath("//a[text()='New Customer']")).click();
+		driver.findElement(By.name("name")).sendKeys(customerName);
+		WebElement dobTextbox = driver.findElement(By.name("dob"));
+		js.executeScript("arguments[0].removeAttribute('type')", dobTextbox);
+		dobTextbox.sendKeys(dob);
+		driver.findElement(By.name("addr")).sendKeys(addressInput);
+		driver.findElement(By.name("city")).sendKeys(city);
+		driver.findElement(By.name("state")).sendKeys(state);
+		driver.findElement(By.name("pinno")).sendKeys(pin);
+		driver.findElement(By.name("telephoneno")).sendKeys(phoneNum);
+		driver.findElement(By.name("emailid")).sendKeys(email);
+		driver.findElement(By.name("password")).sendKeys(password);
+		driver.findElement(By.name("sub")).click();
+
+		Assert.assertEquals(driver.findElement(By.cssSelector("p.heading3")).getText(), "Customer Registered Successfully!!!");
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Customer Name']/following-sibling::td")).getText(), customerName);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Gender']/following-sibling::td")).getText(), gender);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Birthdate']/following-sibling::td")).getText(), dob);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Address']/following-sibling::td")).getText(), addressOutput);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='City']/following-sibling::td")).getText(), city);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='State']/following-sibling::td")).getText(), state);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Pin']/following-sibling::td")).getText(), pin);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Mobile No.']/following-sibling::td")).getText(), phoneNum);
+		Assert.assertEquals(driver.findElement(By.xpath("//td[text()='Email']/following-sibling::td")).getText(), email);
+
+		customerId = driver.findElement(By.xpath("//td[text()='Customer ID']/following-sibling::td")).getText();
+	}
+
+	@Test
+	public void TC_04_Edit_Customer() {
+		driver.findElement(By.xpath("//a[text()='Edit Customer']")).click();
+		driver.findElement(By.name("cusid")).sendKeys(customerId);
+		driver.findElement(By.name("AccSubmit")).click();
+
+		Assert.assertEquals(driver.findElement(By.name("name")).getAttribute("value"), customerName);
+		Assert.assertEquals(driver.findElement(By.name("gender")).getAttribute("value"), gender);
+		Assert.assertEquals(driver.findElement(By.name("dob")).getAttribute("value"), dob);
+
+		Assert.assertEquals(driver.findElement(By.name("addr")).getText(), addressInput);
+		Assert.assertEquals(driver.findElement(By.name("city")).getAttribute("value"), city);
+		Assert.assertEquals(driver.findElement(By.name("state")).getAttribute("value"), state);
+		Assert.assertEquals(driver.findElement(By.name("pinno")).getAttribute("value"), pin);
+		Assert.assertEquals(driver.findElement(By.name("telephoneno")).getAttribute("value"), phoneNum);
+		Assert.assertEquals(driver.findElement(By.name("emailid")).getAttribute("value"), email);
+
+		driver.findElement(By.name("addr")).clear();
+		driver.findElement(By.name("addr")).sendKeys(editAddressInput);
+		driver.findElement(By.name("city")).clear();
+		driver.findElement(By.name("city")).sendKeys(editCity);
+		driver.findElement(By.name("state")).clear();
+		driver.findElement(By.name("state")).sendKeys(editState);
+		driver.findElement(By.name("pinno")).clear();
+		driver.findElement(By.name("pinno")).sendKeys(editPin);
+		driver.findElement(By.name("telephoneno")).clear();
+		driver.findElement(By.name("telephoneno")).sendKeys(editPhone);
+		driver.findElement(By.name("emailid")).clear();
+		driver.findElement(By.name("emailid")).sendKeys(editEmail);
+		driver.findElement(By.name("sub")).click();
+		sleepInSecond(3);
+	}
+
+	@AfterClass
+	public void afterClass() {
+		driver.quit();
+	}
+
+	public int randomNumber() {
+		Random rand = new Random();
+		return rand.nextInt(999);
+	}
+
+	public void sleepInSecond(long second) {
+		try {
+			Thread.sleep(second * 1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
