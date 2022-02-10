3a8d90453b757a8d067671121c9fc57c1b38a057
KhangD <Khang.DaoD@homecredit.vn>
2022-02-10 23:52:43 +0700

    conplete User Interactions

--- selenium/Topic_09_User_Interactions.java
+++ selenium/Topic_09_User_Interactions.java
@@ -17,7 +17,7 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
-public class Topic_09_User_Interactions_Part_I {
+public class Topic_09_User_Interactions {
 	WebDriver driver;
 	Actions action;
 	JavascriptExecutor jsExecutor;
@@ -83,13 +83,30 @@ public class Topic_09_User_Interactions_Part_I {
 	}
 
 	@Test
-	public void TC_05_Double_Click() {
+	public void TC_06_Double_Click() {
 		driver.get("https://automationfc.github.io/basic-form/index.html");
 		By doubleclickButton = By.xpath("//button[text()='Double click me']");
 		jsExecutor.executeScript("arguments[0].scrollIntoView(true);", driver.findElement(doubleclickButton));
 		action.doubleClick(driver.findElement(doubleclickButton)).perform();
 		sleepInSecond(2);
 		Assert.assertEquals(driver.findElement(By.cssSelector("#demo")).getText(), "Hello Automation Guys!");
+	}
+
+	@Test
+	public void TC_07_Right_Click() {
+		driver.get("http://swisnl.github.io/jQuery-contextMenu/demo.html");
+		action.contextClick(driver.findElement(By.xpath("//span[text()='right click me']"))).perform();
+		sleepInSecond(1);
+		action.moveToElement(driver.findElement(By.cssSelector(".context-menu-icon-quit"))).perform();
+		sleepInSecond(1);
+
+		Assert.assertTrue(driver.findElement(By.cssSelector(".context-menu-icon-quit.context-menu-hover")).isDisplayed());
+
+		action.click(driver.findElement(By.cssSelector(".context-menu-icon-quit"))).perform();
+		sleepInSecond(1);
+		driver.switchTo().alert().accept();
+		
+		Assert.assertFalse(driver.findElement(By.cssSelector(".context-menu-icon-quit")).isDisplayed());
 	}
 
 	@AfterClass
